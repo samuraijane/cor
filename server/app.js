6c459bf871863feaaa11fe0f036cc8c97d3b2620
Jack Skiles <jackskiles420@gmail.com>
2022-06-17 13:44:14 -0400

    added google sheets credentials json, created rout to get spreadsheet data

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 var express = require('express');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const {google} = require("googleapis");
 
 var app = express();
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const {google} = require("googleapis");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/google', async function(req, res, next) {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "sheets-credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Client instance auth
+  const client = await auth.getClient();
+  
+  // Google Sheet API instance
+  const googleSheets = google.sheets({ version: "v4", auth: client});
+
+  // Get sheets data
+
+  const spreadsheetId= "1bM7_YyERZx4iKOefjKD3wqXgNWiA9pssUcy-bqi7NWM";
+
+  const data = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  })
+  res.send(data);
+})
 
 module.exports = router;
