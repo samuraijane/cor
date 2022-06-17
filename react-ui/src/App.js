20f10f92cd19a3e6f6dcfb2a5c9f5d7b01c64187
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-06-17 16:50:05 -0400

    avatars

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,12 +2,18 @@ import "./styles/app.scss";
 import Header from "./sectioning/header/header";
 import Main from "./sectioning/main/main";
 import Footer from "./sectioning/footer/footer";
-import SVGImage, { AVATAR } from "./components/svg/icons";
+import SVGImage, { AVATAR, FACEBOOK, GITHUB, GOOGLE, INSTAGRAM } from "./components/svg/icons";
+
 
 function App() {
   return (
     <div className="App">
       <SVGImage type={AVATAR} />
+      <SVGImage type={FACEBOOK} />
+      <SVGImage type={GITHUB} />
+      <SVGImage type={GOOGLE} />
+      <SVGImage type={INSTAGRAM} />
+      
       <Header />
       <Main />
       <Footer />
