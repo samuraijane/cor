5e56ea9bb983ce677ef3aca8481704a63d81fe8b
Jack Skiles <jackskiles420@gmail.com>
2022-06-17 15:28:05 -0400

    changed some credentials data handling to make it an object in index.js file

--- server/sheets-credentials.js
+++ server/sheets-credentials.js
@@ -1,9 +1,4 @@
-var express = require('express');
-var router = express.Router();
-const {google} = require("googleapis");
-
-
-const credentials =
+module.exports =
 {
   type: "service_account",
   project_id: "cityofrefuge-353617",
@@ -16,63 +11,3 @@ const credentials =
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/cityofrefuge%40cityofrefuge-353617.iam.gserviceaccount.com"
 }
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/google', async function(req, res, next) {
-  const auth = new google.auth.GoogleAuth({
-    credentials: credentials,
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-  console.log(auth);
-  // Client instance auth
-  const client = await auth.getClient();
-
-  const rowValues = "Mentee";
-  let spreadsheetId= "1bM7_YyERZx4iKOefjKD3wqXgNWiA9pssUcy-bqi7NWM";
-
-  // if(req.value.role == "Mentor")
-  if(rowValues == "Mentor")
-  {
-    spreadsheetId = "1KjFWSbpAddTq0qgucoqZPZ6sPpWFo4csAjkG3Xl2c-I" 
-  } 
-  // else if(req.value.role == "Mentee")
-  else if(rowValues == "Mentee")
-  {
-    spreadsheetId = "1bM7_YyERZx4iKOefjKD3wqXgNWiA9pssUcy-bqi7NWM" 
-  }
-  
-  // Google Sheet API instance
-  const googleSheets = google.sheets({ version: "v4", auth: client});
-
-  // Get sheets data
-
-  // const data = await googleSheets.spreadsheets.get({
-  // })
-
-  // const getRows = await googleSheets.spreadsheets.values.get({
-  // })
-
-  // Write rows to spreadsheet
-  googleSheets.spreadsheets.values.append({
-    auth,
-    spreadsheetId,
-    range: "Sheet1!A:B",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-
-      values: [
-        //req.body
-        // ["Test20", "test29"],
-        ["Jack", "Skiles", "Bob"],
-      ],
-
-    }
-  })
-  res.send("posted");
-})
-
-module.exports = router;
