692432803d838c1bccba8c8e7c48af2b9770921b
Stephen-Roach <stephenroach21@gmail.com>
2022-06-17 15:44:51 -0500

    implement boiler plate for react router

---
+++ react-ui/src/routes/mentee/mentee.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Mentee() {
+  return <div>I am the Mentee Page.</div>
+}
+
+export default Mentee
--- react-ui/src/sectioning/main/main.jsx
+++ react-ui/src/sectioning/main/main.jsx
@@ -1,65 +1,21 @@
-import React from "react";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import About from '../../routes/about/about'
+import Home from '../../routes/home/home'
+import Mentor from '../../routes/mentor/mentor'
+import Mentee from '../../routes/mentee/mentee'
 
 const Main = () => {
   return (
-    <div className="main">
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-        consequuntur, ullam voluptatum possimus, necessitatibus ipsa aliquid
-        dolore excepturi modi aliquam at, nisi saepe voluptate? Minima dicta
-        dolore cupiditate iure molestiae. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Voluptatum totam voluptates quae nulla enim, dolorem
-        sunt aliquid tempore animi quisquam magni consequatur rem modi sequi!
-        Asperiores magnam sint dicta delectus. Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Voluptates, assumenda temporibus est quasi
-        voluptatibus ducimus quos dicta cum dolor, tempore quia modi, autem
-        quidem sapiente natus molestias at sint pariatur! Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Adipisci voluptatem sapiente tempora
-        quis blanditiis provident, at fugiat corrupti perferendis repudiandae.
-        Unde eos temporibus magni eveniet distinctio in. Facere, perspiciatis
-        alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-        consequuntur, ullam voluptatum possimus, necessitatibus ipsa aliquid
-        dolore excepturi modi aliquam at, nisi saepe voluptate? Minima dicta
-        dolore cupiditate iure molestiae. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Voluptatum totam voluptates quae nulla enim, dolorem
-        sunt aliquid tempore animi quisquam magni consequatur rem modi sequi!
-        Asperiores magnam sint dicta delectus. Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Voluptates, assumenda temporibus est quasi
-        voluptatibus ducimus quos dicta cum dolor, tempore quia modi, autem
-        quidem sapiente natus molestias at sint pariatur! Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Adipisci voluptatem sapiente tempora
-        quis blanditiis provident, at fugiat corrupti perferendis repudiandae.
-        Unde eos temporibus magni eveniet distinctio in. Facere, perspiciatis
-        alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-        consequuntur, ullam voluptatum possimus, necessitatibus ipsa aliquid
-        dolore excepturi modi aliquam at, nisi saepe voluptate? Minima dicta
-        dolore cupiditate iure molestiae. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Voluptatum totam voluptates quae nulla enim, dolorem
-        sunt aliquid tempore animi quisquam magni consequatur rem modi sequi!
-        Asperiores magnam sint dicta delectus. Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Voluptates, assumenda temporibus est quasi
-        voluptatibus ducimus quos dicta cum dolor, tempore quia modi, autem
-        quidem sapiente natus molestias at sint pariatur! Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Adipisci voluptatem sapiente tempora
-        quis blanditiis provident, at fugiat corrupti perferendis repudiandae.
-        Unde eos temporibus magni eveniet distinctio in. Facere, perspiciatis
-        alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-        consequuntur, ullam voluptatum possimus, necessitatibus ipsa aliquid
-        dolore excepturi modi aliquam at, nisi saepe voluptate? Minima dicta
-        dolore cupiditate iure molestiae. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Voluptatum totam voluptates quae nulla enim, dolorem
-        sunt aliquid tempore animi quisquam magni consequatur rem modi sequi!
-        Asperiores magnam sint dicta delectus. Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Voluptates, assumenda temporibus est quasi
-        voluptatibus ducimus quos dicta cum dolor, tempore quia modi, autem
-        quidem sapiente natus molestias at sint pariatur! Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Adipisci voluptatem sapiente tempora
-        quis blanditiis provident, at fugiat corrupti perferendis repudiandae.
-        Unde eos temporibus magni eveniet distinctio in. Facere, perspiciatis
-        alias.
-      </h1>
-    </div>
-  );
-};
+    <main className="main">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="mentor" element={<Mentor />} />
+        <Route path="mentee" element={<Mentee />} />
+      </Routes>
+    </main>
+  )
+}
 
-export default Main;
+export default Main
