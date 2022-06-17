45c5142e5647eca674728b9a6318a86c48ccadcc
Matthew Day <matt@matthewday.net>
2022-06-17 13:31:15 -0400

    correct casing with imports

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,7 +1,7 @@
 import "./styles/app.scss";
-import Header from "./sectioning/header/Header";
-import Main from "./sectioning/main/Main";
-import Footer from "./sectioning/footer/Footer";
+import Header from "./sectioning/header/header";
+import Main from "./sectioning/main/main";
+import Footer from "./sectioning/footer/footer";
 
 function App() {
   return (
