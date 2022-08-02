263522799d0369431aab75f2ee47ab305c40f4f8
Matthew Day <matt@matthewday.net>
2022-08-01 23:10:25 -0400

    remove default react stylesheet

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ViewportProvider } from './context/viewportContext';
 import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
